2383e99ce7302b650bc9488f9ac190840e5e849c
Smythy <smythy.costa@hotmail.com>
2018-04-02 14:55:53 -0300

    bizzu sobre rotas, criado um novo controller para Users

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function (){
-	Route::get('/products', 'ProductController@index');
-	Route::post('/products', 'ProductController@store');
-	Route::put('/products/{product}', 'ProductController@update');
-	Route::get('/products/{product}', 'ProductController@show');
-	Route::delete('/products/{product}', 'ProductController@destroy');
+
+
+	//////////////////////////////////////
+	//  | Exibindo todas as rotas
+	//////////////////////////////////////
+	//Route::get('/products', 'ProductController@index');
+	//Route::post('/products', 'ProductController@store');
+	//Route::put('/products/{product}', 'ProductController@update');
+	//Route::get('/products/{product}', 'ProductController@show');
+	//Route::delete('/products/{product}', 'ProductController@destroy');
+	
+
+	//////////////////////////////////////
+	//  | Simplificando a todas as Rotas
+	//  | com apenas uma linha.
+	//////////////////////////////////////
+	// Route::resources('products', 'ProductController');
+
+
+	Route::resources([
+		'products' => 'ProductController',
+		'users' => 'UsersController.php',
+	]);
+
 });
