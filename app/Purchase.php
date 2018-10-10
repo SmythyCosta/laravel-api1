29875d11e09f7c8f7d8f482551b0b842d8bb8e7c
smythycosta <smythy.costa@gmail.com>
2018-10-10 17:30:16 -0300

    model purchase

---
+++ app/Purchase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class Purchase extends Model
+{
+	protected $table = "purchase";
+}
