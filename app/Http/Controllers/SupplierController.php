2534344ee4dabe715f55cf03dbf08d73d115d865
Smythy Costa <smythy.costa@hotmail.com>
2018-10-11 16:19:22 -0300

    SupplierController methodo getSupplier

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -20,6 +20,20 @@ class SupplierController extends Controller
         return response()->json(['status'=>200,'supplier'=>$all]); 
     }
 
-
+    public function getSupplier(Request $request)
+    {
+        $id = $request->input('id');
+        $supplier = Supplier::find($id); 
+        $data = [
+            'id' => $supplier->id,
+            'company' => $supplier->company,
+            'name' => $supplier->name,
+            'email' => $supplier->email,
+            'phone' => $supplier->phone,
+            'status' => $supplier->status,
+            'image' => base64_encode($supplier->image)
+        ];          
+        return response()->json(['status'=>200,'supplier'=>$data]); 
+    }
 
 }
