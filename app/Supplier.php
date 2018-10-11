a231a4fcb3e4821fc990ada92fcea52696a103c4
Smythy Costa <smythy.costa@hotmail.com>
2018-10-11 16:13:55 -0300

    criando o model Supplier

---
+++ app/Supplier.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class Supplier extends Model
+{
+    //
+    protected $table = "supplier";
+
+
+}
