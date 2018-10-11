8092d7be76ef7804049373fce677dc75ac384333
Smythy Costa <smythy.costa@hotmail.com>
2018-10-11 17:04:40 -0300

    model Customer funcao allCustomer

--- app/Customer.php
+++ app/Customer.php
@@ -3,10 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class Customer extends Model
 {
 
     protected $table = 'customer';
+
+    public function allCustomer()
+    {
+        return DB::table('customer')
+                        ->select('id','name','email','phone','address','status')
+                        ->orderBy('id','DESC')
+                        ->get();
+    }
 
 }
