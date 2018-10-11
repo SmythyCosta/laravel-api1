7683d038d70e586b23789ff6679e6b87790c6381
Smythy Costa <smythy.costa@hotmail.com>
2018-10-11 16:18:38 -0300

    SupplierController methodo getAllSupplier

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -14,4 +14,12 @@ use App\Models\Setting;
 class SupplierController extends Controller
 {
     //
+    public function getAllSupplier(Request $request)
+    {
+        $all = Supplier::select('id','company','name','email','phone','status')->orderBy('id','DESC')->get();           
+        return response()->json(['status'=>200,'supplier'=>$all]); 
+    }
+
+
+
 }
