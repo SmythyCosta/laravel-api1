514775e77c1af69829b980ed501fb2397c7eafa7
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 09:48:37 -0300

    pdf PurchasePDF Header

--- app/LibPDF/PurchasePDF.php
+++ app/LibPDF/PurchasePDF.php
@@ -13,4 +13,18 @@ class PurchasePDF extends baseFpdf{
 		parent::__construct($orientation, $unit, $size);
 	}
 
+	function Header(){
+		if($this->PageNo() != 1){
+			$this->SetFont('Arial','B',12);
+	        $this->cell(15,6,"SL",1,"","C");
+	        $this->cell(35,6,"Purchase Code",1,"","C");
+	        $this->cell(45,6,"Supplier Name",1,"","C");
+	        $this->cell(35,6,"Date",1,"","C");
+	        $this->cell(45,6,"Amount",1,"","C");
+	        $this->cell(35,6,"Due",1,"","C");
+	        $this->cell(35,6,"Payment Type",1,"","C");
+			$this->Ln();
+		}
+	}
+
 }
