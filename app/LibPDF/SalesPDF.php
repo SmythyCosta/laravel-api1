66b2dec3941f8366ebe9a83c77cf664dbe58c58c
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 09:58:10 -0300

    pdf SalesPDF footer

--- app/LibPDF/SalesPDF.php
+++ app/LibPDF/SalesPDF.php
@@ -31,4 +31,13 @@ class SalesPDF extends baseFpdf{
         $this->cMargin = $margin;
     }
 
+    function Footer(){
+	    // Position at 1.5 cm from bottom
+	    $this->SetY(-15);
+	    // Arial italic 8
+	    $this->SetFont('Arial','I',8);
+	    // Page number
+	    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+	}
+
 }
