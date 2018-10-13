cf1c49051357fb33f65b0349dd1c0e363fe39e7f
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 09:50:21 -0300

    footer

--- app/LibPDF/PurchasePDF.php
+++ app/LibPDF/PurchasePDF.php
@@ -32,4 +32,13 @@ class PurchasePDF extends baseFpdf{
         $this->cMargin = $margin;
     }
 
+	function Footer(){
+	    // Position at 1.5 cm from bottom
+	    $this->SetY(-15);
+	    // Arial italic 8
+	    $this->SetFont('Arial','I',8);
+	    // Page number
+	    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+	}
+
 }
