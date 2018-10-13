b1e5c751c4b4820e591efc4405869d91df12a959
Smythy Costa <smythy.costa@hotmail.com>
2018-10-12 23:09:40 -0300

    pdf CustomerLib metodo Header

--- app/LibPDF/CustomerLib.php
+++ app/LibPDF/CustomerLib.php
@@ -13,6 +13,21 @@ class CustomerLib extends baseFpdf{
         $size = 'A4';
         parent::__construct($orientation, $unit, $size);
     }
-    
+
+    function Header()
+    {
+        if($this->PageNo() != 1){
+
+            $this->SetFont('Arial','B',12);
+            $this->cell(15,6,"SL",1,"","C");
+            $this->cell(45,6,"Name",1,"","C");
+            $this->cell(50,6,"Email",1,"","C");
+            $this->cell(30,6,"Phone",1,"","C");
+            $this->cell(50,6,"Address",1,"","C");
+            $this->cell(20,6,"Status",1,"","C");
+            $this->Ln(); // Line break
+
+        }   
+    }
 
 }
