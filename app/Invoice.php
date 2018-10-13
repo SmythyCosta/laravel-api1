2f2dc0bc43c3a864d33ca7cc25fa0502cec2eec7
Smythy Costa <smythy.costa@hotmail.com>
2018-10-12 22:51:37 -0300

    model invoice metodo getAllSales

--- app/Invoice.php
+++ app/Invoice.php
@@ -9,4 +9,15 @@ class Invoice extends Model
     //
     protected $table = "invoice";
 
+    public function getAllSales()
+    {
+    	return DB::table('invoice')
+                        ->join('customer','invoice.customer_id','=','customer.id')
+                        ->join('payment','invoice.id','=','payment.invoice_id')
+                        ->select(DB::raw('DATE_FORMAT(invoice.created_at,"%d %M %Y") as date'),DB::raw('sum(payment.amount) as totalAmount'),'payment.payment_type','invoice.id','invoice.invoice_code','invoice.due','customer.name as customerName')
+                        ->groupBy('invoice.id')
+                        ->orderBy('invoice.id', 'desc')
+                        ->get();
+    }
+
 }
