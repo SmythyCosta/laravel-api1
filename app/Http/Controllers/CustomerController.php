716ca98a902c121acf45fa564673c20ecb43904f
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 11:52:32 -0300

    controller CustomerController metodo customerSave

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,5 +11,28 @@ use App\Customer;
 
 class CustomerController extends Controller
 {
-	
+
+    public function customerSave(Request $request)
+    {
+        $customer = new Customer;
+        $customer->name = $request->input('name');
+        $customer->phone = $request->input('phone');
+        $customer->email = $request->input('email');
+        $customer->address = $request->input('address');
+        $customer->discount_percentage = $request->input('discount_percentage');
+        $customer->status = $request->input('status');
+        $customer->gender = $request->input('gender');
+        $customer->created_at = date('Y-m-d');
+        
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            // Get the contents of the file
+            $contents = $file->openFile()->fread($file->getSize());
+            $customer->image = $contents;
+        }
+
+        $customer->save();
+        return response()->json(['status'=>200,'mesg'=>'Customer Save Success']); 
+    }
+
 }
