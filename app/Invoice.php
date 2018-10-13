bde16de9a52a81b3a67f5d769c2712713aea3ae0
Smythy Costa <smythy.costa@hotmail.com>
2018-10-12 22:52:28 -0300

    model invoice metodo salesReportData

--- app/Invoice.php
+++ app/Invoice.php
@@ -20,4 +20,23 @@ class Invoice extends Model
                         ->get();
     }
 
+    public function salesReportData(Request $request)
+    {
+        $fromDate = $request->input('fromdate');
+        $toDate = $request->input('todate');
+
+        $report =   DB::table('invoice')
+                            ->join('customer','invoice.customer_id','=','customer.id')
+                            ->join('payment','invoice.id','=','payment.invoice_id')
+                            ->select(DB::raw('DATE_FORMAT(invoice.created_at,"%d %M %Y") as date'),'invoice.due','invoice.invoice_code','payment.payment_type',DB::raw('sum(payment.amount) as amount'),'customer.name')
+                            ->where('payment.type','income')
+                            ->whereRaw("invoice.created_at >= '".$fromDate." 00:00:00' AND invoice.created_at <= '".$toDate." 00:00:00'")
+                            ->groupBy('invoice.id')
+                            ->orderBy('invoice.id','DESC')
+                            ->get();
+
+        return $report; 
+    }
+    
+
 }
