e13dc2ce45a8a15d3209940d95000c454028db5e
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 09:20:29 -0300

    pdf OrderPDF metodo Header

--- app/LibPDF/OrderPDF.php
+++ app/LibPDF/OrderPDF.php
@@ -13,4 +13,19 @@ class OrderPDF extends baseFpdf{
 		parent::__construct($orientation, $unit, $size);
 	}
 
+	function Header(){
+		if($this->PageNo() != 1){
+			$this->SetFont('Arial','B',12);
+	        $this->cell(15,6,"SL",1,"","C");
+	        $this->cell(35,6,"Order Code",1,"","C");
+	        $this->cell(45,6,"Customer Name",1,"","C");
+	        $this->cell(35,6,"Date",1,"","C");
+	        $this->cell(45,6,"Amount",1,"","C");
+	        $this->cell(35,6,"Due",1,"","C");
+	        $this->cell(35,6,"Payment Type",1,"","C");
+			$this->Ln();
+		}
+	}
+
+	
 }
