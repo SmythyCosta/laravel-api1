b707970f4d5a69200088bb57eec9e66c59bb1b56
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 09:57:03 -0300

    pdf SalesPDF header

--- app/LibPDF/SalesPDF.php
+++ app/LibPDF/SalesPDF.php
@@ -11,5 +11,19 @@ class SalesPDF extends baseFpdf{
 		$size = 'A4';
 		parent::__construct($orientation, $unit, $size);
 	}
-	
+
+	function Header(){
+		if($this->PageNo() != 1){
+			$this->SetFont('Arial','B',12);
+	        $this->cell(15,6,"SL",1,"","C");
+	        $this->cell(35,6,"Invoice Code",1,"","C");
+	        $this->cell(45,6,"Customer Name",1,"","C");
+	        $this->cell(35,6,"date",1,"","C");
+	        $this->cell(35,6,"Amount",1,"","C");
+	        $this->cell(35,6,"Due",1,"","C");
+	        $this->cell(35,6,"Payment Type",1,"","C");
+			$this->Ln();
+		}
+	}
+
 }
