d8d19b6e741028d09b034bd18fb46a3a1643c218
Smythy Costa <smythy.costa@hotmail.com>
2018-10-12 22:55:57 -0300

    model UserRole

---
+++ app/UserRole.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class UserRole extends Model
+{
+    //
+    protected $table = "user_role";
+
+}
