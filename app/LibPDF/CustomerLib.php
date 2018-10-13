82c63470d57e079f92eadb40b0828acb45dfb3cc
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 09:16:52 -0300

    correcao no design do codigo

--- app/LibPDF/CustomerLib.php
+++ app/LibPDF/CustomerLib.php
@@ -6,18 +6,15 @@ use Anouar\Fpdf\Fpdf as baseFpdf;
 
 class CustomerLib extends baseFpdf{
 
-    public function __construct()
-    {
+    public function __construct(){
         $orientation = 'L';
         $unit = 'mm';
         $size = 'A4';
         parent::__construct($orientation, $unit, $size);
     }
 
-    function Header()
-    {
+    function Header(){
         if($this->PageNo() != 1){
-
             $this->SetFont('Arial','B',12);
             $this->cell(15,6,"SL",1,"","C");
             $this->cell(45,6,"Name",1,"","C");
@@ -26,7 +23,6 @@ class CustomerLib extends baseFpdf{
             $this->cell(50,6,"Address",1,"","C");
             $this->cell(20,6,"Status",1,"","C");
             $this->Ln(); // Line break
-
         }   
     }
 
@@ -35,8 +31,7 @@ class CustomerLib extends baseFpdf{
         $this->cMargin = $margin;
     }
 
-    function Footer()
-    {
+    function Footer(){
         // Position at 1.5 cm from bottom
         $this->SetY(-15);
         // Arial italic 8
@@ -44,6 +39,6 @@ class CustomerLib extends baseFpdf{
         // Page number
         $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
     }
-    
+
 
 }
--- app/LibPDF/DamagedProductPDF.php
+++ app/LibPDF/DamagedProductPDF.php
@@ -8,20 +8,14 @@ class DamagedProductPDF extends baseFpdf{
 
 
     public function __construct(){
-
         $orientation = 'L';
         $unit = 'mm';
         $size = 'A4';
-
         parent::__construct($orientation, $unit, $size);
-
     }
 
-
     function Header(){
-
         if($this->PageNo() != 1){
-
             $this->SetFont('Arial','B',12);
             $this->cell(25,6,"SL",1,"","C");
             $this->cell(35,6,"Product Code",1,"","C");
@@ -32,27 +26,21 @@ class DamagedProductPDF extends baseFpdf{
             $this->cell(35,6,"Date",1,"","C");
             //$this->Cell(35,5,'User','TRB',0,'C');
             $this->Ln(); // Line break
-
         }
-
     }
-
 
     function SetCellMargin($margin){
         // Set cell margin
         $this->cMargin = $margin;
     }
 
-
     function Footer(){
-
         // Position at 1.5 cm from bottom
         $this->SetY(-15);
         // Arial italic 8
         $this->SetFont('Arial','I',8);
         // Page number
         $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-
     }
 
 
