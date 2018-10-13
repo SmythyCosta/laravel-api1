d10dacf95774ac6a495ffe98846b25e94a1253bc
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 10:10:16 -0300

    pdf UserPDF header

--- app/LibPDF/UserPDF.php
+++ app/LibPDF/UserPDF.php
@@ -12,4 +12,16 @@ class UserPDF extends baseFpdf{
 		parent::__construct($orientation, $unit, $size);
 	}
 
+	function Header(){
+		if($this->PageNo() != 1){
+			$this->SetFont('Arial','B',12);
+	        $this->cell(25,6,"ID",1,"","C");
+	        $this->cell(45,6,"Name",1,"","L");
+	        $this->cell(45,6,"Email",1,"","L");
+	        $this->cell(45,6,"Phone",1,"","L");
+	        $this->cell(35,6,"Address",1,"","L");
+			$this->Ln();
+		}
+	}
+
 }
