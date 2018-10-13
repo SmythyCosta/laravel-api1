452c0ef82caa0036fb3ffad77b16c0efe26e9067
Smythy Costa <smythy.costa@hotmail.com>
2018-10-13 09:19:19 -0300

    pdf OrderPDF metodo __construct

--- app/LibPDF/OrderPDF.php
+++ app/LibPDF/OrderPDF.php
@@ -6,4 +6,11 @@ use Anouar\Fpdf\Fpdf as baseFpdf;
 
 class OrderPDF extends baseFpdf{
 	
+	public function __construct(){
+		$orientation = 'L';
+		$unit = 'mm';
+		$size = 'A4';
+		parent::__construct($orientation, $unit, $size);
+	}
+
 }
