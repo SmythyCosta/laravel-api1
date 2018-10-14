4cc78f7676aa070be9c7c5aa730f4d9b1c368b23
Smythy Costa <smythy.costa@hotmail.com>
2018-10-14 12:21:11 -0300

    controller EmployeeController employeeSave

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -8,8 +8,22 @@ use Illuminate\Support\Facades\DB;
 
 //Imports Models
 use App\Employee;
+use App\User;
 
 class EmployeeController extends Controller
 {
 
+    public function employeeSave(Request $request)
+    {
+
+        $employee = new User;
+        $employee->name = $request->input('name');
+        $employee->email = $request->input('email');
+        $employee->phone = $request->input('phone');
+        $employee->address = $request->input('address');
+        $employee->password = bcrypt($request->input('password'));
+        $employee->save();
+
+        return response()->json(['status'=>200,'mesg'=>'Employee Save Success']); 
+    }
 }
