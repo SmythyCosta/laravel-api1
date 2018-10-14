67edf54b6f7563ee2d1e2c238fa57d00ac214e49
Smythy Costa <smythy.costa@hotmail.com>
2018-10-14 14:06:27 -0300

    controller PurchaseController metodo getPurchasesDetails

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 use App\Product;
 use App\Purchase; 
 use App\Supplier;
+use App\Setting;
 
 class PurchaseController extends Controller{
     
@@ -81,5 +82,46 @@ class PurchaseController extends Controller{
         DB::table('payment')->insert($payment);
         return response()->json(['status'=>200,'purchase_id'=>$insert_id]);
     }
+
+    public function getPurchasesDetails(Request $request)
+    {
+        $setting = new Setting;  
+        $settingData = $setting->settingData(); 
+
+        $id = $request->input('id');
+        $purchase =  DB::table('purchase')
+                        ->join('supplier', 'purchase.supplier_id', '=', 'supplier.id')
+                        ->select('purchase.id',DB::raw('DATE_FORMAT(purchase.created_at,"%d %M %Y") as date'),'purchase.purchase_code','purchase.due','purchase.purchase_info','supplier.company','supplier.name','supplier.email','supplier.phone')
+                        ->where('purchase.id',$id)
+                        ->first(); 
+
+        $payment = DB::table('payment')->select(DB::raw('sum(amount) as totalAmount'))->where('purchase_id',$purchase->id)->first();
+
+        $data['company']       =  $purchase->company;                                
+        $data['name']          =  $purchase->name;                                
+        $data['email']         =  $purchase->email;                                
+        $data['phone']         =  $purchase->phone;                                
+        $data['purchase_code'] =  $purchase->purchase_code;                                
+        $data['date']          =  $purchase->date;                                
+        $data['due']           =  $purchase->due;
+        $data['totalAmount']      =  $payment->totalAmount;
+        $product = [];  
+        $purchase_info =json_decode($purchase->purchase_info);
+        $subtotal = 0;                          
+        foreach ($purchase_info as $key => $value) {
+            $product_info = DB::table('product')->select('serial_number','name')->where('id',$value->id)->first();
+            $product['product_id'] = $value->id;
+            $product['product_name'] = $product_info->name;
+            $product['serial_number'] = $product_info->serial_number;
+            $product['quantity'] = $value->quantity;
+            $product['purchase_price'] = $value->purchase_price;
+            $data['purchase_info'][] = $product;
+            $subtotal += $value->quantity * $value->purchase_price;
+        }
+        $data['subtotal'] = $subtotal;
+        $data['settingData'] = $settingData;
+        return response()->json(['status'=>200,'purchase' => $data]);
+    }
+
 
 }	
