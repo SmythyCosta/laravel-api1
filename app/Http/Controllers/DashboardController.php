d6e35519c6a4a5b9036e361a472a0b4b834b63a5
Smythy Costa <smythy.costa@hotmail.com>
2018-10-14 10:13:00 -0300

    controller  DashboardController metodo getAllTotalCount

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,4 +13,16 @@ use App\Setting;
 class DashboardController extends Controller
 {
 
+	public function getAllTotalCount(Request $request)
+	{
+		$product = DB::table('product')->select(DB::raw('count(id) as count'))->first();
+		$damaged_product = DB::table('damaged_product')->select(DB::raw('sum(id) as count'))->first();
+		$customer = DB::table('customer')->select(DB::raw('count(id) as count'))->first();
+		$user = DB::table('users')->select(DB::raw('count(id) as count'))->first();
+		$supplier = DB::table('supplier')->select(DB::raw('count(id) as count'))->first();
+		$category = DB::table('category')->select(DB::raw('count(id) as count'))->first();
+		
+		return response()->json(['status'=>200,'product'=>$product,'damaged_product'=>$damaged_product,'customer'=>$customer,'user'=>$user,'supplier'=>$supplier,'category'=>$category]);
+	}
+	
 }
