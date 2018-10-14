87025115d69a9238a903f8a1835c56ef07f6a39f
Smythy Costa <smythy.costa@hotmail.com>
2018-10-14 14:52:29 -0300

    classe controller ReportController

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//Imports Laravel
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+//Imports Models
+
+//libs
+use Excel;
+
+class ReportController extends Controller
+{
+
+}
