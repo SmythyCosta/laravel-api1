280412f438220b4b6266341909609c3631c41dbe
Smythy Costa <smythy.costa@hotmail.com>
2018-10-14 11:26:06 -0300

    controller EmployeeController

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//Imports Laravel
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+//Imports Models
+use App\Employee;
+
+class EmployeeController extends Controller
+{
+
+}
