d1626b1c90c99a016528e6e48a0554d2bbd1e6cb
Smythy Costa <smythy.costa@hotmail.com>
2018-10-14 11:10:24 -0300

    controller  DashboardController metodo getAllDashboardData

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,5 +24,17 @@ class DashboardController extends Controller
 		
 		return response()->json(['status'=>200,'product'=>$product,'damaged_product'=>$damaged_product,'customer'=>$customer,'user'=>$user,'supplier'=>$supplier,'category'=>$category]);
 	}
+
+	public function getAllDashboardData(Request $request)
+	{
+		$endDate = date("Y-m-31").' 00:00:00';
+		$startDate = date('Y-m-01').' 00:00:00';
 	
+		$invoice = DB::table('invoice')->select(DB::raw('sum(grand_total) as total'))->whereBetween(DB::raw('date(created_at)'), [$startDate, $endDate])->first();
+		$payment = DB::table('payment')->select(DB::raw('sum(amount) as total'))->where('type','income')->whereBetween(DB::raw('date(created_at)'), [$startDate, $endDate])->first();
+		$expense = DB::table('payment')->select(DB::raw('sum(amount) as total'))->where('type','expense')->whereBetween(DB::raw('date(created_at)'), [$startDate, $endDate])->first();
+
+		return response()->json(['status'=>200,'sales'=>$invoice,'payment'=>$payment,'expense'=>$expense]);
+	}
+
 }
