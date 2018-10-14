e5cb990aceccfb3c7bc29a5f67a37a2faaa5b829
Smythy Costa <smythy.costa@hotmail.com>
2018-10-14 12:51:56 -0300

    controller PurchaseController metodo createNewPurchase

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -50,4 +50,36 @@ class PurchaseController extends Controller{
         return response()->json(['status'=>200,'purchase'=>$all]);
     }
 
+    public function createNewPurchase(Request $request)
+    {
+        $product = $request->input('products');
+        $purchase['purchase_code'] = $request->input('purchaseCode');
+        $purchase['supplier_id'] = $request->input('supplierName');
+        $purchase['due'] = $request->input('due');
+        $purchase['created_at'] = $request->input('date');
+
+        for ($i=0; $i <count($product) ; $i++) {
+            $products[] = [
+                'id' => $product[$i]['id'],
+                'quantity' => $product[$i]['quantity'],
+                'purchase_price' => $product[$i]['purchase_price'],
+            ];
+            $quantity = Product::find($product[$i]['id']);
+            $quantity->stock_quantity = $quantity->stock_quantity + $product[$i]['quantity'];
+            $quantity->save();
+        }
+
+        $purchase['purchase_info']   =   json_encode($products);
+        $insert_id = DB::table('purchase')->insertGetId($purchase);
+        $payment['purchase_id'] = $insert_id;
+        $payment['amount'] = $request->input('receivedAmount');
+        $payment['type']   =   'expense';
+        $payment['supplier_id'] = $request->input('supplierName');
+        $payment['payment_type'] = $request->input('paymentType');
+        $payment['created_at'] = $request->input('date');
+
+        DB::table('payment')->insert($payment);
+        return response()->json(['status'=>200,'purchase_id'=>$insert_id]);
+    }
+
 }	
