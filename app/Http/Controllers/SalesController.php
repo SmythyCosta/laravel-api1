969ab85becdefa8cdb6840655d6b3435f24dfd5c
Smythy Costa <smythy.costa@hotmail.com>
2018-10-16 19:09:24 -0300

    controller Sales

---
+++ app/Http/Controllers/SalesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//Imports Laravel
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+//Imports Models
+use App\Product;
+use App\Purchase; 
+use App\Invoice;
+use App\Setting; 
+use App\LibPDF\SalesPDF;
+
+//libs
+use Excel;
+
+class SalesController extends Controller
+{
+	
+}
