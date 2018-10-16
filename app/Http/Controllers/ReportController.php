960fe99e88623ef43e553b9e45055581f1c9b096
Smythy Costa <smythy.costa@hotmail.com>
2018-10-16 19:07:02 -0300

    controller report purchaseReportDownloadExcel

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -206,4 +206,77 @@ class ReportController extends Controller
         exit;
     }
 
+    public function purchaseReportDownloadExcel(Request $request)
+    {          
+        $setting = Setting::where('id',1)->first();
+    
+        Excel::create('purchase-report', function ($excel) use ($request)   {
+           
+            $excel->setTitle('Purchase Report');
+            $excel->sheet('Purchase Report', function ($sheet) use ($request) {
+                
+                // first row styling and writing content
+                $sheet->mergeCells('A1:E1');
+                $sheet->row(1, function ($row) {
+                    $row->setFontFamily('Comic Sans MS');
+                    $row->setFontSize(30);
+                    // $row->setBorder('solid', 'none', 'none', 'solid');
+                });
+                // $sheet->setBorder('A1:F1', 'thin');
+
+                // Set all borders (top, right, bottom, left)
+                // $cells->setBorder('solid', 'none', 'none', 'solid');
+                $sheet->row(1, array('Purchase Report'));
+
+                $sheet->setColumnFormat(array( //se formatea la columna a texto
+                    'B' => \PHPExcel_Style_NumberFormat::FORMAT_NUMBER,
+                ));
+                
+                // getting data to display - in my case only one record
+                // setting column names for data - you can of course set it manually
+                $sheet->appendRow(2,
+                                    array(
+                                        'SL',
+                                        'Purchase Code',
+                                        'Supplier Name',
+                                        'Amount',
+                                        'Date',
+                                        'Due',
+                                        'Payment Type'
+                                        )
+                                    );
+
+                // getting last row number (the one we already filled and setting it to bold
+                $sheet->row($sheet->getHighestRow(), function ($row) {
+                    $row->setFontWeight('bold');
+                    $row->setAlignment('center');
+                    $row->setBorder('thin', 'thin', 'thin', 'thin');
+                });
+
+                $purchase = new Purchase();
+                $report = $purchase->purchaseReportData($request);
+
+                // putting users data as next rows
+                foreach ($report as $key => $data) {
+                    $sheet->appendRow(
+                                    array(
+                                            $key+1,
+                                            $data->purchase_code,
+                                            $data->company,
+                                            $data->amount,
+                                            $data->date,
+                                            $data->due,
+                                            (($data->payment_type==1) ? 'cash' : ($data->payment_type==2) ? 'check' : 'card')
+                                        )
+                                    );
+                    $sheet->row($sheet->getHighestRow(), function ($row) {
+                        $row->setAlignment('center');
+                        $row->setBorder('thin', 'thin', 'thin', 'thin');
+                    });
+                }
+            });
+
+        })->export('xls');
+    }
+
 }
