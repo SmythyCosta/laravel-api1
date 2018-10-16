b1175542c898300b18b35b97dc9e2788c31a620a
Smythy Costa <smythy.costa@hotmail.com>
2018-10-16 19:12:02 -0300

    controller Sales metodo createNewSales

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -34,4 +34,41 @@ class SalesController extends Controller
         return response()->json(['status'=>200,'product'=>$product]);
     } 
 
+    public function createNewSales(Request $request)
+    {
+        $product = $request->input('products');
+        $sales['invoice_code'] = $request->input('invoiceCode');
+        $sales['discount_percentage'] = $request->input('discount');
+        $sales['vat_percentage'] = $request->input('vat');
+        $sales['sub_total'] = $request->input('subTotal');
+        $sales['grand_total'] = $request->input('grandTotal');
+        $sales['due'] = $request->input('due');
+        $sales['customer_id'] = $request->input('customer');
+        $sales['created_at'] = $request->input('date');
+
+        for ($i=0; $i <count($product) ; $i++) {
+            $products[] = [
+                'id' => $product[$i]['id'],
+                'quantity' => $product[$i]['quantity'],
+                'selling_price' => $product[$i]['selling_price'],
+            ];
+            $quantity = Product::find($product[$i]['id']);
+            $quantity->stock_quantity = $quantity->stock_quantity - $product[$i]['quantity'];
+            $quantity->save();
+
+        }
+        
+        $sales['invoice_entries']   =   json_encode($products);
+        $insert_id = DB::table('invoice')->insertGetId($sales);
+        $payment['invoice_id'] = $insert_id;
+        $payment['amount'] = $request->input('receivedAmount');
+        $payment['type']   =   'income';
+        $payment['customer_id'] = $request->input('customer');
+        $payment['payment_type'] = $request->input('paymentType');
+        $payment['created_at'] = $request->input('date');
+        DB::table('payment')->insert($payment);
+
+        return response()->json(['status'=>200,'sales_id'=>$insert_id]);
+    }
+
 }
