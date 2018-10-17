197224db8e7a0e864f2b5b127db2d491dd830756
Smythy Costa <smythy.costa@hotmail.com>
2018-10-16 23:41:08 -0300

    controller Setting metodo settingUpdate

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -32,4 +32,26 @@ class SettingController extends Controller
         ];
          return response()->json(['status'=>200,'setting'=>$data]);
     }
+
+    public function settingUpdate(Request $request)
+    {
+        $id = 1;
+        $setting = Setting::find($id);
+        $setting->company_name = $request->input('company_name');
+        $setting->address = $request->input('address');
+        $setting->phone = $request->input('phone');
+        $setting->email = $request->input('email');
+        $setting->currency = $request->input('currency');
+        $setting->vat_percentage = $request->input('vat_percentage');
+        $setting->discount_percentage = $request->input('discount_percentage');
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            // Get the contents of the file
+            $contents = $file->openFile()->fread($file->getSize());
+            $setting->image = $contents;
+        }
+        $setting->save();
+        
+        return response()->json(['status'=>200,'mesg'=>'Setting Update Success']);
+    }
 }
