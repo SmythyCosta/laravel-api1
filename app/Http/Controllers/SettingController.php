853940e6afe03c886ad378c5badef78846a8eb1a
Smythy Costa <smythy.costa@hotmail.com>
2018-10-16 23:40:17 -0300

    controller Setting metodo getSetting

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -18,4 +18,18 @@ use JWTAuth;
 class SettingController extends Controller
 {
 
+    public function getSetting(Request $request)
+    {  
+        $find = Setting::where('id',1)->first();  
+        $data = [
+            'company_name' => $find->company_name,
+            'address' => $find->address,
+            'phone' => $find->phone,
+            'email' => $find->email,
+            'vat_percentage' => $find->vat_percentage,
+            'currency' => $find->currency,
+            'image' => base64_encode($find->image)
+        ];
+         return response()->json(['status'=>200,'setting'=>$data]);
+    }
 }
