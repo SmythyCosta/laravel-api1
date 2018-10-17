73c0f191db1a0a5c16f968938edf6eef3d34b8b7
Smythy Costa <smythy.costa@hotmail.com>
2018-10-16 23:39:05 -0300

    controller Setting

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//Imports Laravel
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+//Imports Models
+use App\Setting;
+use App\Menu; 
+use App\Submenu;
+
+//libs
+use Excel;
+use JWTAuth;
+
+class SettingController extends Controller
+{
+
+}
