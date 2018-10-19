7a0315967bbb318973cc460fed67e39770465642
Smythy Costa <smythy.costa@hotmail.com>
2018-10-19 07:35:19 -0300

    construtor

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+//Imports Models
+use App\Models\Setting;
+use App\Menu;
+use App\Submenu;
+use App\UserRole;
+use App\User;
+use App\LibPDF\UserPDF;
+
+
+//libs
+use Excel;
+use Hash;
+use JWTAuth;
+use JWTAuthException;
 
 
 class UserController extends Controller
 {
+
+	private $user;
+    
+    public function __construct(User $user){
+        $this->user = $user;
+    }
 
 }
