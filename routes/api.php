01b797c150b2a80814a0829f99d6fb3b7b848c8f
Smythy Costa <smythy.costa@hotmail.com>
2018-10-26 20:39:08 -0300

    refactory routes

--- routes/api.php
+++ routes/api.php
@@ -13,79 +13,49 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// ====================== UserController ======================
+Route::post('user-create', 'UserController@userCreate');
+Route::post('user-login', 'UserController@login');
+
+// route middleware jwt
+Route::group(['middleware' => 'jwt.auth'], function () {
+    Route::post('get-user', 'UserController@getAuthUser');
 });
 
 
-Route::prefix('v1')->group(function (){
+// ====================== Category ======================
+Route::get('get-all-category','CategoryController@getAllCategory');
+Route::post('category-save','CategoryController@categorySave');
+Route::post('get-category','CategoryController@getCategory');
+Route::post('category-update','CategoryController@categoryUpdate');
+Route::post('category-delete','CategoryController@categoryDelete');
+Route::get('get-all-category-by-grid','CategoryController@getAllCategoryByGrid');
+Route::post('get-cat-by-subCategory','CategoryController@getCatBySubCategory');
+Route::post('subCategory-save','CategoryController@subCategorySave');
+Route::post('get-subCategory','CategoryController@getSubCategory');
+Route::post('subCategory-update','CategoryController@subCategoryUpdate');
+Route::post('subCategory-delete','CategoryController@subCategoryDelete');
+Route::get('get-subCategoryGridData','CategoryController@subCategoryGridData');
 
 
-    //////////////////////////////////////
-    //  | Exibindo todas as rotas
-    //////////////////////////////////////
-    //Route::get('/products', 'ProductController@index');
-    //Route::post('/products', 'ProductController@store');
-    //Route::put('/products/{product}', 'ProductController@update');
-    //Route::get('/products/{product}', 'ProductController@show');
-    //Route::delete('/products/{product}', 'ProductController@destroy');
+// ====================== Product ======================
+Route::post('product-save','ProductController@productSave');
+Route::get('all-product','ProductController@allProduct');
+Route::post('get-product-details','ProductController@getProduct');
+Route::post('product-update','ProductController@productUpdate');
+Route::post('get-product-info','ProductController@getProductInfo');
+Route::get('product-list-pdf','ProductController@exportpdf');
 
 
-    //////////////////////////////////////
-    //  | Simplificando a todas as Rotas
-    //  | com apenas uma linha.
-    //////////////////////////////////////
-    // Route::resources('products', 'ProductController');
+// ====================== DamagedProduct ======================
+Route::post('damaged-product-save','DamagedProductController@productSave');
+Route::get('all-damaged-product','DamagedProductController@allDamagedProduct');
+Route::post('get-all-product-by-damaged','DamagedProductController@allProduct');
+Route::post('get-damaged-product','DamagedProductController@getDamagedProduct');
+Route::post('damaged-product-update','DamagedProductController@productUpdate');
+Route::get('damaged-product-list-pdf','DamagedProductController@exportpdf');
+// ====================== end DamagedProduct ======================
 
 
-    //////////////////////////////////////
-    //  | Grupo de Rotas
-    //////////////////////////////////////
-    Route::resources([
-        //'products' => 'ProductController',
-        'users' => 'UsersController',
-    ]);
-
-
-    // ====================== Category ======================
-    Route::get('get-all-category','CategoryController@getAllCategory');
-    Route::post('category-save','CategoryController@categorySave');
-    Route::post('get-category','CategoryController@getCategory');
-    Route::post('category-update','CategoryController@categoryUpdate');
-    Route::post('category-delete','CategoryController@categoryDelete');
-    Route::get('get-all-category-by-grid','CategoryController@getAllCategoryByGrid');
-
-    Route::post('get-cat-by-subCategory','CategoryController@getCatBySubCategory');
-    Route::post('subCategory-save','CategoryController@subCategorySave');
-    Route::post('get-subCategory','CategoryController@getSubCategory');
-    Route::post('subCategory-update','CategoryController@subCategoryUpdate');
-    Route::post('subCategory-delete','CategoryController@subCategoryDelete');
-    Route::get('get-subCategoryGridData','CategoryController@subCategoryGridData');
-    // ====================== end Category ======================
-
-
-    // ====================== Product ======================
-    Route::post('product-save','ProductController@productSave');
-    Route::get('all-product','ProductController@allProduct');
-    Route::post('get-product-details','ProductController@getProduct');
-    Route::post('product-update','ProductController@productUpdate');
-    Route::post('get-product-info','ProductController@getProductInfo');
-    Route::get('product-list-pdf','ProductController@exportpdf');
-    // ====================== end Product ======================
-
-
-    // ====================== DamagedProduct ======================
-    Route::post('damaged-product-save','DamagedProductController@productSave');
-    Route::get('all-damaged-product','DamagedProductController@allDamagedProduct');
-    Route::post('get-all-product-by-damaged','DamagedProductController@allProduct');
-    Route::post('get-damaged-product','DamagedProductController@getDamagedProduct');
-    Route::post('damaged-product-update','DamagedProductController@productUpdate');
-    Route::get('damaged-product-list-pdf','DamagedProductController@exportpdf');
-    // ====================== end DamagedProduct ======================
-
-
-    // ====================== Passport ======================
-    Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
-
-
-});
+// ====================== Passport ======================
+Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
